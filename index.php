92f7119b014afbec98455476304e36da8cdf75fb
tonyvizoso <tonyv@adgraphprint.com>
2015-07-10 10:57:55 -0400

    Added basic index fil

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+	<title>Welcome to Ocean Blue Medical Research Center</title>
+</head>
+<body>
+ <p>Welcome to Ocean Blue Medical Research Center</p>
+</body>
+</html>
