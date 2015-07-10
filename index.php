2988cdcecc95ab81b6293505cc7fc05f4cdb3c98
tonyvizoso <tonyv@adgraphprint.com>
2015-07-10 14:20:26 -0400

    Installed Bootstrap template

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8" />
-	<title>Welcome to Ocean Blue Medical Research Center</title>
-</head>
-<body>
- <p>Welcome to Ocean Blue Medical Research Center</p>
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    
+    <script src="_/js/bootstrap.js"></script>
+    <script src="_/js/myscript.js"></script>
+  </body>
 </html>
