c6a7896312d15e45c6d640b940c57a4809877e4a
tonyvizoso <tonyv@adgraphprint.com>
2015-09-15 16:26:29 -0400

    update

--- page_content/ceo-words.php
+++ page_content/ceo-words.php
@@ -1,2 +1,6 @@
 <h3 class="orange italic bold">A message from our CEO</h3>
+<figure>
+<img src="images/staff/yanet-r-ferrera_small.jpg">
+<figcaption class="center"><br>Yanet R. Ferrera, CEO<br><em>Ocean Blue Medical Research Center</em></figcaption>
+</figure>
 <p><span class="bold">Ocean Blue Medical Research Center</span> is a multi-specialty clinical research center who conducts clinical trials from Phase I to Phase IV. These studies involve, among many other, therapeutic areas such as: Cardiology, Neurology, Psychiatry, Internal Medicine, Dermatology, Endocrinology, Infectious disease, Rheumatology and Women's health.</p>
