a3d1e8089d77de66fb672a29286722d618482ba6
tonyvizoso <tonyv@adgraphprint.com>
2015-09-15 16:06:46 -0400

    co.php update, page_content update and configcodekit

--- ceo.php
+++ ceo.php
@@ -31,9 +31,7 @@
             <?php include "page_content/ceo-content.php" ?>
           </section><!-- main -->
           <section class="sidebar col col-lg-4">
-            <h3 class="orange italic bold">A message from our CEO</h3>
-            <p><span class="bold">Ocean Blue Medical Research Center</span> is a multi-specialty clinical research center who conducts clinical trials from Phase I to Phase IV. These studies involve, among many other, therapeutic areas such as: Cardiology, Neurology, Psychiatry, Internal Medicine, Dermatology, Endocrinology, Infectious disease, Rheumatology and Women's health.</p>
-
+            <?php include "page_content/ceo-words" ?>
           </section><!-- sidebar -->
       </div><!-- content -->
           <?php include "_/components/php/footer.php" ?>
---
+++ page_content/ceo-words.php
@@ -0,0 +1,2 @@
+<h3 class="orange italic bold">A message from our CEO</h3>
+<p><span class="bold">Ocean Blue Medical Research Center</span> is a multi-specialty clinical research center who conducts clinical trials from Phase I to Phase IV. These studies involve, among many other, therapeutic areas such as: Cardiology, Neurology, Psychiatry, Internal Medicine, Dermatology, Endocrinology, Infectious disease, Rheumatology and Women's health.</p>
