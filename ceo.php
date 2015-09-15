7152dce225ecafca8705172145fd07d179a8b409
tonyvizoso <tonyv@adgraphprint.com>
2015-09-15 16:18:42 -0400

    update

--- ceo.php
+++ ceo.php
@@ -29,6 +29,7 @@
         </section><!-- header -->
           <section class="main col col-lg-8">
             <?php include "page_content/ceo-content.php" ?>
+            <div class="dividerBottom"></div>
           </section><!-- main -->
           <section class="sidebar col col-lg-4">
             <?php include "page_content/ceo-words" ?>
