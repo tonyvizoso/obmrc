17b2e008c0156a5ca4522e07a6c47f2f8aca32e2
tonyvizoso <tonyv@adgraphprint.com>
2015-09-21 18:22:01 -0400

    update

--- page_content/ceo-words.php
+++ page_content/ceo-words.php
@@ -3,10 +3,10 @@
 <img src="images/staff/yanet-r-ferrera_small.jpg">
 <figcaption class="center">Yanet R. Ferrera, CEO<br><em>Ocean Blue Medical Research Center</em></figcaption>
 </figure>
-<p>When we started to walk the long road to make <span class="bold">Ocean Blue Medical Research Center</span> a strong and trustfull research facility, we knew that paving that route wasn't going to be an easy task.
+<p>When we started to walk the long road to make <span class="bold">Ocean Blue Medical Research Center</span> a strong and trustful research facility, we knew that paving that route wasn't going to be an easy task.</p>
 
-We have been able to gather the best team of qualified professionals and supporters. It was a big challenge because in this career - in addition to qualifications -, the self-motivation, discipline, endurance, and self-preparedness are very important factors for being successful as investigator or coordinator.
+<p>We have been able to gather the best team of qualified professionals and supporters. It was a big challenge because in this career - in addition to qualifications -, the self-motivation, discipline, endurance, and self-preparedness are very important factors for being successful as investigator or coordinator.</p>
 
-The proper training of our personnel as well as in participating volunteers, has been another big goal reached by <span class="bold">Ocean Blue Medical Research Center.</span> The increasing number of new studies are proof of how deep we dig to set our reputation and how solid our infrastructure today.
+<p>The proper training of our personnel as of in the participating volunteers, has been another big goal reached by <span class="bold">Ocean Blue Medical Research Center.</span> The increasing number of new studies are proof of how deep we dig to set our reputation and how solid our infrastructure today.</p>
 
-I, as a founder and CEO of <span class="bold">Ocean Blue Medical Research Center</span>, feel very proud of our team. With effort and dedication, this group of enthusiasts has committed to continue developing never ending trail of hard work and process where people's health is our biggest dream.</p>
+<p>I, as a founder and CEO of <span class="bold">Ocean Blue Medical Research Center</span>, feel very proud of our team. With effort and dedication, this group of enthusiastic professionals has committed to continue the never ending development trail where people's health is our biggest dream.</p>
