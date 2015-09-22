595694d1b37af8b0b3af8398936a93d3c34f7b55
tonyvizoso <tonyv@adgraphprint.com>
2015-09-22 17:00:22 -0400

    update

--- page_content/ceo-words.php
+++ page_content/ceo-words.php
@@ -7,6 +7,6 @@
 
 <p>We have been able to gather the best team of qualified professionals and supporters. It was a big challenge because in this career - in addition to qualifications -, the self-motivation, discipline, endurance, and self-preparedness are very important factors for being successful as investigator or coordinator.</p>
 
-<p>The proper training of our personnel as of in the participating volunteers, has been another big goal reached by <span class="bold">Ocean Blue Medical Research Center.</span> The increasing number of new studies are proof of how deep we dig to set our reputation and how solid our infrastructure today.</p>
+<p>The proper training of our personnel as of in the participating volunteers, has been another big goal reached by <span class="bold">Ocean Blue Medical Research Center.</span> The increasing number of new studies are proof of how deep we dig to set our reputation on a solid foundation with a modern infrastructure, accordingly to the today industry's highest standards.</p>
 
 <p>I, as a founder and CEO of <span class="bold">Ocean Blue Medical Research Center</span>, feel very proud of our team. With effort and dedication, this group of enthusiastic professionals has committed to continue the never ending development trail where people's health is our biggest dream.</p>
